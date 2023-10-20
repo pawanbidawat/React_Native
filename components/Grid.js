22366c7edf5d0aeda8bf399cf99dcec7a9d65c8d
PawanBidawat <pawanbidawat404@gmail.com>
2023-10-20 10:31:00 +0530

    Grid Example With [map(), flex property]

---
+++ components/Grid.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {View, Text, StyleSheet ,FlatList, ScrollView} from 'react-native';
+
+const Grid = () => {
+
+  const users = [
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1},
+    {name : "sam",
+    id:1}
+  ];
+  return(
+    <View>
+      <Text style={{fontSize:30,textAlign:'center' , marginTop:60}}>Grid With map function and flex property</Text>
+   
+    <View style={{marginBottom:100, flex:1, flexDirection:'row',flexWrap:'wrap'}}>
+    
+      {
+        users.map((detail)=><Text style={[styles.item,styles.data]}>{detail.name}</Text>)
+      }
+      
+      </View>
+      
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create(
+  { item : {
+    backgroundColor:'black',
+    padding:10,
+    color:'white',
+    marginBottom:10,
+    width:98,
+    height:80
+  },
+  data:{
+    borderRadius:20,
+    textAlignVertical:'center',
+    textAlign:'center',
+    fontSize:20
+  }
+}
+)
+
+export default Grid;
