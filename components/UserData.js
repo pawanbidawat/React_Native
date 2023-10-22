1f844aa25b4f3507ceb7fcc8a26511f9eb311967
pawanbidawat <94060332+pawanbidawat@users.noreply.github.com>
2023-10-22 10:57:39 +0530

    Add files via upload
    Make list with FlateList and Array.
    Apply style,
    Make component,
    Use component inside List

---
+++ components/UserData.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text , StyleSheet} from 'react-native';
+export default UserData = (props) => {
+    const item = props.data;
+   
+    return(
+      <View style={styles.box}>
+            <Text style={styles.item}>{item.name}</Text>
+            <Text style={styles.item}> {item.id}</Text>
+          </View>
+    )
+  }
+  const styles = StyleSheet.create(
+    {
+      item:{
+        fontSize:20,
+        color:"green",
+        flex:1,
+        borderWidth:2,
+        textAlign:"center",
+        textAlignVertical:"center",
+      },
+      box :{
+        flexDirection:"row",
+        borderWidth:2,
+        borderColor:"red",
+        marginBottom:10
+      }
+    }
+  )
